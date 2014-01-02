031d8d562260ecc43b367de070f1266cfea601e5
Mauro Soria <maurosoria@gmail.com>
2014-01-02 19:06:12 -0300

    deleted pyc files

--- dirs3arch.py
+++ dirs3arch.py
@@ -1,58 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
-from optparse import OptionParser, OptionGroup
-from Fuzzer import *
-from Request import *
-from FuzzerDictionary import *
-from Output import *
+from lib.core import *
+#from Fuzzer import *
+#from Request import *
+#from FuzzerDictionary import *
+#from Output import *
 
 class Program:
     def __init__(self):
         self.script_path = (os.path.dirname(os.path.realpath(__file__)))
-        usage = "Usage: %prog [-u|--url] target [-e|--extensions] extensions [options]"
-        parser = OptionParser(usage)
-        # Mandatory arguments
-        mandatory = OptionGroup(parser, 'Mandatory')
-        mandatory.add_option("-u", "--url", help="URL target", action="store", type="string", dest="url", default=None)
-        mandatory.add_option("-e", "--extensions", help="Extensions list separated by comma (Example: php, asp)", \
-            action="store", dest="extensions", default=None)
-
-        # Optional Settings
-        settings = OptionGroup(parser, 'Optional Settings')
-        settings.add_option("-t", "--threads", help="Number of Threads", action="store", type="int", \
-            dest="threadsCount", default=10)
-        settings.add_option("-x", "--exclude-500", help="Exclude Internal Server Error Status (500)", action="store_true", \
-            dest="exclude500", default=False)
-        settings.add_option("--cookie", "--cookie", action="store", type="string", dest="cookie", default="")
-        settings.add_option("--user-agent", "--user-agent", action="store", type="string", dest="useragent", \
-            default="")
-        settings.add_option("-w", "--wordlist", action="store", dest="wordlist", default=("%s/db/dicc.txt" % (self.script_path)))
-        settings.add_option("-l", "--lowercase", action="store_true", dest="lowercase", default="False")
-        settings.add_option("--ignore-response-status", "--ignore-response-status", action="store", type="string", dest="ignoreResponseStatus", default="")
-
-        parser.add_option_group(mandatory)
-        parser.add_option_group(settings)
-        (options, arguments) = parser.parse_args()
-        if options.url == None:
-            print("Url target is missing")
-            exit(0)
-        if options.extensions == None:
-            print("No extension specified. You must specify at least one extension")
-            exit(0)
-        try:
-            with open(options.wordlist): pass
-        except IOError:
-            print ("Invalid wordlist file")
-            exit(0)
-        self.url = options.url
-        self.extensions = [extension.strip() for extension in options.extensions.split(",")]
-        self.useragent = options.useragent
-        self.cookie = options.cookie
-        self.threadsCount = options.threadsCount
-        self.exclude500 = options.exclude500
-        self.wordlist = options.wordlist
-        self.lowercase = options.lowercase
+        self.arguments = ArgumentsParser(self.script_path)
 
 
     def run(self):
@@ -64,17 +22,19 @@ class Program:
             pass
         output.printHeader(programBanner)
         output.printHeader("version 0.2\n")
-        output.printWarning("- Searching in: {0}".format(self.url))
-        output.printWarning("- For extensions: {0}".format(', '.join(self.extensions)))
-        output.printWarning("- Number of Threads: {0}\n".format(self.threadsCount))
-        requester = Requester(self.url, self.cookie, self.useragent)
-        dictionary = FuzzerDictionary(self.wordlist, self.extensions, self.lowercase)
-        
-        fuzzer = Fuzzer(requester, dictionary, output, threads = self.threadsCount, \
-            excludeInternalServerError = self.exclude500)
+        output.printWarning("- Searching in: {0}".format(self.arguments.url))
+        output.printWarning("- For extensions: {0}".format(', '.join(self.arguments.extensions)))
+        output.printWarning("- Number of Threads: {0}\n".format(self.arguments.threadsCount))
         try:
+            requester = Requester(self.arguments.url, cookie = self.arguments.cookie, useragent = self.arguments.useragent, maxPool = self.arguments.threadsCount, maxRetries = self.arguments.maxRetries, timeout = self.arguments.timeout, ip = self.arguments.ip)
+            dictionary = FuzzerDictionary(self.arguments.wordlist, self.arguments.extensions, self.arguments.lowercase)
+            fuzzer = Fuzzer(requester, dictionary, output, threads = self.arguments.threadsCount, \
+                excludeInternalServerError = self.arguments.exclude500)
             fuzzer.start()
             fuzzer.wait()
+        except RequestException as e:
+            output.printError("Unexpected error:\n{0}".format(e.args[0]['message']))
+            exit(0)
         except KeyboardInterrupt:
             output.printError("\nCanceled by the user")
             exit(0)
