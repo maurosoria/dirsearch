4e5a0dbb1ece3c9a16f777ce0e536b84842c980e
Mauro Soria <maurosoria@gmail.com>
2014-07-17 19:52:26 -0300

    Changed version

--- dirs3arch.py
+++ dirs3arch.py
@@ -34,7 +34,7 @@ class Program(object):
 if __name__ == '__main__':
     MAYOR_VERSION = 0
     MINOR_VERSION = 2
-    REVISION = 3
+    REVISION = 4
     PROGRAM_BANNER = \
     r"""         _ _            _____                  _     
       __| (_)_ __ ___  |___ /    __ _ _ __ ___| |__  
