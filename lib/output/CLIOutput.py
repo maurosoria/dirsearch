186dc7038e38faf3c7d85855986c236708fdc6e7
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-09-13 12:15:54 +0700

    Update CLIOutput.py

--- lib/output/CLIOutput.py
+++ lib/output/CLIOutput.py
@@ -157,7 +157,7 @@ class CLIOutput(object):
     def error(self, reason):
         with self.mutex:
             stripped = reason.strip()
-            message = "\n"
+            message = "\n" if "\n" in reason else ""
             message += Style.BRIGHT + Fore.WHITE + Back.RED
             message += stripped
             message += Style.RESET_ALL
