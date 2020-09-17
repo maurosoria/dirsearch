28cad35ce86b960490ff85fc92830f222c73c415
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-09-17 17:18:01 +0700

    Bug fix

--- lib/output/CLIOutput.py
+++ lib/output/CLIOutput.py
@@ -148,7 +148,7 @@ class CLIOutput(object):
             message = "{0:.2f}% - ".format(percentage(index, length))
             
             if len(allJobs) > 1:
-                message += "Job: {1}/{2} - ".format(currentJob, allJobs)
+                message += "Job: {0}/{1} - ".format(currentJob, allJobs)
 
             if self.errors > 0:
                 message += "Errors: {0} - ".format(self.errors)
