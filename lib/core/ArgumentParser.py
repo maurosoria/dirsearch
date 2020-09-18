8ef84d1a8a94ad5f49dbcc9b09ef5e22c7b0d200
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-09-18 13:16:41 +0700

    Before release dirsearch v0.4.0 =)

--- lib/core/ArgumentParser.py
+++ lib/core/ArgumentParser.py
@@ -361,7 +361,7 @@ class ArgumentParser(object):
 
     def parseArguments(self):
         usage = "Usage: %prog [-u|--url] target [-e|--extensions] extensions [options]"
-        parser = OptionParser(usage, epilog='''
+        parser = OptionParser(usage, epilog='''Note:
 You can change the dirsearch default configurations (default extensions, timeout, wordlist location, ...) by editing the "default.conf" file. More information at https://github.com/maurosoria/dirsearch.                          
 ''')
         # Mandatory arguments
