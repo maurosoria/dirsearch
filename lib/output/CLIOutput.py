8d13d312caa2d2ba9ed43a9bc069a3ddbd1db91a
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-10-09 22:54:32 +0700

    Update CLIOutput.py

--- lib/output/CLIOutput.py
+++ lib/output/CLIOutput.py
@@ -38,7 +38,6 @@ class CLIOutput(object):
         self.lastInLine = False
         self.mutex = threading.Lock()
         self.blacklists = {}
-        self.mutexCheckedPaths = threading.Lock()
         self.basePath = None
         self.errors = 0
 
@@ -99,7 +98,7 @@ class CLIOutput(object):
             finally:
                 contentLength = FileUtils.sizeHuman(size)
 
-            if self.basePath is None:
+            if not self.basePath:
                 showPath = urljoin("/", path)
 
             else:
@@ -149,7 +148,6 @@ class CLIOutput(object):
             x, y = get_terminal_size()
 
             message = "{0:.2f}% - ".format(percentage(index, length))
-            
 
             if allJobs > 1:
                 message += "Job: {0}/{1} - ".format(currentJob, allJobs)
