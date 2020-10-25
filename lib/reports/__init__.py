38a756dcbca1dbccabf1d4b8fc297bbca5584367
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-10-25 14:50:00 +0700

    Update for XML report

--- lib/reports/__init__.py
+++ lib/reports/__init__.py
@@ -1,4 +1,5 @@
 from .base_report import *  # noqa: F401
 from .json_report import *  # noqa: F401
+from .xml_report import *  # noqa: F401
 from .plain_text_report import *  # noqa: F401
 from .simple_report import *  # noqa: F401
