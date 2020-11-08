bb343dfba93472729bcedb3da98ddab9d88ef8fa
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-11-08 13:05:01 +0700

    Markdown report

--- lib/reports/__init__.py
+++ lib/reports/__init__.py
@@ -1,5 +1,6 @@
 from .base_report import *  # noqa: F401
 from .json_report import *  # noqa: F401
 from .xml_report import *  # noqa: F401
+from .markdown_report import *  # noqa: F401
 from .plain_text_report import *  # noqa: F401
 from .simple_report import *  # noqa: F401
