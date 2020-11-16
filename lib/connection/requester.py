a36edcb94de45e2d0040045762ea372452d7ec64
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-11-16 18:06:54 +0700

    Quote the base path

--- lib/connection/requester.py
+++ lib/connection/requester.py
@@ -71,6 +71,7 @@ class Requester(object):
             raise RequestException({"message": "Unsupported URL scheme: {0}".format(parsed.scheme)})
 
         self.basePath = parsed.path.lstrip("/")
+        self.basePath = urllib.parse.quote(self.basePath, safe=":/^{}<>|%\"\\")
         self.protocol = parsed.scheme
         self.host = parsed.netloc.split(":")[0]
 
