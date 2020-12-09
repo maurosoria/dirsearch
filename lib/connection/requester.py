95f6e155fc50a103ba694620c3211990fd19a12a
Mauro Soria <maurosoria@gmail.com>
2020-12-09 06:47:01 -0300

    Update requester.py

--- lib/connection/requester.py
+++ lib/connection/requester.py
@@ -128,7 +128,7 @@ class Requester(object):
             self.protocol,
             self.host if self.requestByHostname else self.ip,
             self.port,
-            self.basePath if self.basePath.startswith("/") else self.basePath[-1:],
+            self.basePath if self.basePath.startswith("/") else "/" + self.basePath,
             "" if self.basePath.endswith("/") else "/"
         )
 
