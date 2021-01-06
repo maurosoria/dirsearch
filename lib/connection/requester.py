49805ac33e18aa53b61613628e33772f9b5210d6
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-01-06 22:22:40 +0700

    Update the User-Agent

--- lib/connection/requester.py
+++ lib/connection/requester.py
@@ -28,7 +28,7 @@ from .response import *
 
 class Requester(object):
     headers = {
-        "User-agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36",
+        "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
         "Accept-Language": "*",
         "Accept-Encoding": "*",
         "Keep-Alive": "300",
