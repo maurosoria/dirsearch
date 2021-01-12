24a42a5522b1ad7443390bea56a2fde494a701b4
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-01-12 18:33:35 +0700

    Update __init__.py

--- lib/core/__init__.py
+++ lib/core/__init__.py
@@ -3,3 +3,4 @@ from .dictionary import *  # noqa: F401
 from .fuzzer import *  # noqa: F401
 from .path import *  # noqa: F401
 from .report_manager import *  # noqa: F401
+from .raw import *  # noqa: F401
