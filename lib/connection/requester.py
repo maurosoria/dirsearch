7697930b3edf8411c2efac08d5df12f5ceb1ab69
laxa <laxa@ddracepro.net>
2021-03-01 14:26:42 +0100

    Fix -b option and proxy settings

--- lib/connection/requester.py
+++ lib/connection/requester.py
@@ -75,7 +75,7 @@ class Requester(object):
         # Resolve DNS to decrease overhead
         if ip:
             self.ip = ip
-        else:
+        elif not requestByHostname:
             try:
                 self.ip = socket.gethostbyname(self.host)
             except socket.gaierror:
@@ -152,7 +152,7 @@ class Requester(object):
                         proxy = "http://" + proxy
 
                     if proxy.startswith("http:"):
-                        proxies = {"http": proxy}
+                        proxies = {"http": proxy, "https": proxy}
                     elif proxy.startswith("https:"):
                         proxies = {"https": proxy}
                     else:
