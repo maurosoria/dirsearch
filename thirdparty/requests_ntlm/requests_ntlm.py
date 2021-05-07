5c0defaaad0de51bcb255f4cb9e0000de1cc4866
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-05-07 13:47:19 +0700

    But fix

--- thirdparty/requests_ntlm/requests_ntlm.py
+++ thirdparty/requests_ntlm/requests_ntlm.py
@@ -6,9 +6,9 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.exceptions import UnsupportedAlgorithm
-from ntlm_auth import ntlm
 from thirdparty.requests.auth import AuthBase
 from thirdparty.requests.packages.urllib3.response import HTTPResponse
+from .ntlm_auth import ntlm
 
 
 class HttpNtlmAuth(AuthBase):
