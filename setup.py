5d8f87206480f5029fdc32e1a4476732f095a979
qumusabel <qumusabel@nomail>
2021-05-27 00:43:07 +0500

    Make setup.py wrap dirsearch into a package To resolve: maurosoria/dirsearch#736

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 import setuptools
+import os
+import tempfile
+import shutil
 
+env_dir = tempfile.mkdtemp(prefix='dirsearch-install-')
+shutil.copytree(os.path.abspath(os.getcwd()),
+                os.path.join(env_dir, "dirsearch"))
+print("Created setup.py env in {}".format(env_dir))
+
+os.chdir(env_dir)
+
+print(setuptools.find_packages())
+#input(">> Continue <<")
 setuptools.setup(
     name="dirsearch",
     version="0.4.1",
@@ -7,21 +19,26 @@ setuptools.setup(
     author_email="maurosoria@protonmail.com",
     description="Advanced web path scanner",
     url="https://github.com/maurosoria/dirsearch",
+
     packages=setuptools.find_packages(),
-    py_modules=["dirsearch"],
-    package_dir={"dirsearch": "../dirsearch"},
     entry_points={
-        "console_scripts": ["dirsearch=dirsearch:Program"]
+        "console_scripts": ["dirsearch=dirsearch.dirsearch:Program"]
     },
-    package_data={"": ["../default.conf", "../lib/controller/banner.txt", "../db/*"]},
+
+    package_data={
+        "dirsearch": ["*"]
+    },
     include_package_data=True,
+
     python_requires=">=3.0",
     install_requires=["certifi>=2020.11.8", "chardet>=3.0.2", "urllib3>=1.21.1", "cryptography>=2.8", "PySocks"],
+
     classifiers=[
         "Programming Language :: Python",
         "Environment :: Console",
         "License :: OSI Approved :: GNU General Public License",
         "Operating System :: OS Independent",
         "Topic :: Security"
-    ],
+    ]
 )
+
