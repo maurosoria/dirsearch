c546bc37c2d954e3cf32d0d40647c1455a397e26
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-05-28 20:39:54 +0700

    Update header

--- lib/controller/controller.py
+++ lib/controller/controller.py
@@ -88,7 +88,7 @@ class Controller(object):
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
                 "Accept-Language": "*",
                 "Accept-Encoding": "*",
-                "Keep-Alive": "300",
+                "Keep-Alive": "timeout=15, max=1000",
                 "Cache-Control": "max-age=0",
             }
 
