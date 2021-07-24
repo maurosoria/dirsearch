2333ec92a1458d6c46b2af5feb9a8343906ced6d
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-07-24 18:15:27 +0700

    Remove useless packages in thirdparty folder

--- thirdparty/requests_ntlm/requests_ntlm.py
+++ thirdparty/requests_ntlm/requests_ntlm.py
@@ -2,12 +2,13 @@ import binascii
 import sys
 import warnings
 
+from urllib3.response import HTTPResponse
+
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.exceptions import UnsupportedAlgorithm
 from thirdparty.requests.auth import AuthBase
-from thirdparty.requests.packages.urllib3.response import HTTPResponse
 from thirdparty.ntlm_auth import ntlm
 
 
