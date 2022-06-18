097a85856572868bc63fa90fd6acd42d9615a993
shelld3v <59408894+shelld3v@users.noreply.github.com>
2022-06-18 20:54:18 +0700

    Fixed unit tests

--- tests/utils/test_common.py
+++ tests/utils/test_common.py
@@ -18,7 +18,7 @@
 
 from unittest import TestCase
 
-from lib.utils.common import uniq, get_valid_filename
+from lib.utils.common import merge_path, uniq, get_valid_filename
 
 
 class TestCommonUtils(TestCase):
@@ -29,5 +29,5 @@ class TestCommonUtils(TestCase):
         self.assertEqual(get_valid_filename("http://example.com:80/foobar"), "http___example.com_80_foobar", "Invalid filename for Windows")
 
     def test_merge_path(self):
-        self.assertEqual(merge_path("http://example.com/foo", "bar"), "http://example.com/bar", "The result is unintended")
-        self.assertEqual(merge_path("http://example.com/folder/", "foo/../bar/./"), "http://example.com/folder/foo/bar/", "The result is unintended")
+        self.assertEqual(merge_path("http://example.com/foo", "bar"), "http://example.com/bar")
+        self.assertEqual(merge_path("http://example.com/folder/", "foo/../bar/./"), "http://example.com/folder/bar/")
