8da29c4113f1d44f18e6de6159eeeb13d9193e07
4shen0ne <4shen.01@gmail.com>
2024-10-18 10:21:41 +0800

    lint

--- lib/controller/controller.py
+++ lib/controller/controller.py
@@ -330,7 +330,7 @@ class Controller:
         if not port:
             port = STANDARD_PORTS.get(parsed.scheme, None)
         elif not 0 < port < 65536:
-            raise InvalidURLException(f"Invalid port number: {port}")            
+            raise InvalidURLException(f"Invalid port number: {port}")
 
         if options["ip"]:
             cache_dns(parsed.hostname, port, options["ip"])
