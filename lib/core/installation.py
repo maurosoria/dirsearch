a344de973ffe69cb19b86fa116168bd202ff1772
Pham Sy Minh <59408894+shelld3v@users.noreply.github.com>
2025-10-20 16:42:59 +0700

    Self-implement dependencies checker

--- lib/core/installation.py
+++ lib/core/installation.py
@@ -23,9 +23,6 @@ import subprocess
 import sys
 import importlib.metadata
 
-from packaging.specifiers import SpecifierSet
-from packaging.version import Version
-
 from lib.core.exceptions import FailedDependenciesInstallation, MissingDependencies
 from lib.core.settings import SCRIPT_PATH
 from lib.utils.file import FileUtils
@@ -33,12 +30,19 @@ from lib.utils.file import FileUtils
 REQUIREMENTS_FILE = f"{SCRIPT_PATH}/requirements.txt"
 
 
+# Return 1 if a > b, -1 if a < b and 0 if a == b
+def compare_versions(a, b):
+    pa = [int(x) for x in a.split(".")]
+    pb = [int(x) for x in b.split(".")]
+    return (pa > pb) - (pa < pb)
+
+
 def get_dependencies() -> list[str]:
     try:
         return [
-            line.strip()
+            line.replace(" ", "")
             for line in FileUtils.get_lines(REQUIREMENTS_FILE)
-            if not line.lstrip().startswith("#")
+            if line.strip() and not line.lstrip().startswith("#")
         ]
     except FileNotFoundError:
         print("Can't find requirements.txt")
@@ -48,16 +52,28 @@ def get_dependencies() -> list[str]:
 # Check if all dependencies are satisfied
 def check_dependencies() -> None:
     for pkg in get_dependencies():
-        pkg_name = pkg.split("[")[0].split()[0]
+        pkg_name = pkg.split("[")[0]
 
         try:
             installed_version = importlib.metadata.version(pkg_name)
         except importlib.metadata.PackageNotFoundError:
             raise MissingDependencies
 
-        required_version = pkg.split("]")[-1].split(pkg_name)[-1]
-        if Version(installed_version) not in SpecifierSet(required_version):
-            raise MissingDependencies
+        # Check if installed version sastisfies the requirement
+        required_version = pkg.split("]")[-1].split(pkg_name)[-1].lstrip("<>!=~")
+        if (
+            ("=" in pkg and "!" not in pkg and required_version == installed_version)
+            or (">" in pkg and compare_versions(required_version, installed_version) == 1)
+            or ("<" in pkg and compare_versions(required_version, installed_version) == -1)
+            or (
+                "~" in pkg
+                and compare_versions(required_version, installed_version) == 1
+                and required_version.split(".")[0] == installed_version.split(".")[0]
+            )
+        ):
+            continue
+
+        raise MissingDependencies
 
 
 def install_dependencies() -> None:
