9df8f51e1137f836802ff4bd01fc46b5416ab50c
devdev-automation <137927318+devdev-automation@users.noreply.github.com>
2026-01-23 10:15:12 -0500

    Refactor install_requires to read from requirements.txt
    Updated the setup.py to dynamically parse requirements from requirements.txt and removed hardcoded install_requires.

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 import io
 import os
-import setuptools
 import shutil
 import tempfile
 
-from lib.core.settings import VERSION
+import pkg_resources
+import setuptools
 
+from lib.core.settings import VERSION
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(current_dir, "README.md"), encoding="utf-8") as fd:
     desc = fd.read()
+
+# Source - https://stackoverflow.com/questions/49689880/proper-way-to-parse-requirements-file-after-pip-upgrade-to-pip-10-x-x
+# Posted by sinoroc, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-23, License - CC BY-SA 4.0
+
+with io.open(os.path.join(current_dir, "requirements.txt"), encoding="utf-8") as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement 
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 env_dir = tempfile.mkdtemp(prefix="dirsearch-install-")
 shutil.copytree(os.path.abspath(os.getcwd()), os.path.join(env_dir, "dirsearch"))
@@ -30,24 +42,7 @@ setuptools.setup(
     package_data={"dirsearch": ["*", "db/*"]},
     include_package_data=True,
     python_requires=">=3.9",
-    install_requires=[
-        "PySocks",
-        "Jinja2",
-        "defusedxml",
-        "pyopenssl",
-        "requests",
-        "requests_ntlm",
-        "colorama",
-        "ntlm_auth",
-        "beautifulsoup4",
-        "mysql-connector-python",
-        "psycopg[binary]",
-        "defusedcsv",
-        "requests-toolbelt",
-        "setuptools",
-        "httpx",
-        "httpx-ntlm"
-    ],
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python",
         "Environment :: Console",
