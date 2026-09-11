f1f24cf98f643a0562aaa2c1269a24efcf2cd6b7
mauro <maurosoria@gmail.com>
2026-09-11 14:59:09 +0000

    Expand IPv6 target format coverage

--- tests/controller/test_target_url.py
+++ tests/controller/test_target_url.py
@@ -26,7 +26,30 @@ class TestControllerTargetURL(TestCase):
 
     def test_ipv6_target_authority_keeps_literal_brackets(self):
         cases = (
+            ("http://[::]/", "http://[::]/"),
             ("http://[::1]/", "http://[::1]/"),
+            (
+                "http://[0:0:0:0:0:0:0:1]/",
+                "http://[0:0:0:0:0:0:0:1]/",
+            ),
+            (
+                "http://[2001:0db8:0000:0000:0000:ff00:0042:8329]/",
+                "http://[2001:0db8:0000:0000:0000:ff00:0042:8329]/",
+            ),
+            (
+                "http://[2001:db8::ff00:42:8329]/",
+                "http://[2001:db8::ff00:42:8329]/",
+            ),
+            ("http://[2001:db8::]/", "http://[2001:db8::]/"),
+            ("http://[2001:DB8::ABCD]/", "http://[2001:db8::abcd]/"),
+            (
+                "http://[::ffff:192.0.2.128]/",
+                "http://[::ffff:192.0.2.128]/",
+            ),
+            (
+                "http://[64:ff9b::192.0.2.33]/",
+                "http://[64:ff9b::192.0.2.33]/",
+            ),
             (
                 "https://[2001:db8::1]:443/private",
                 "https://[2001:db8::1]/",
@@ -51,3 +74,13 @@ class TestControllerTargetURL(TestCase):
                 self.controller.requester.set_url.assert_called_once_with(
                     expected_url
                 )
+
+    def test_scheme_option_keeps_ipv6_literal_brackets(self):
+        options["scheme"] = "https"
+
+        self.controller.set_target("[2001:db8::1]:8443/private")
+
+        self.assertEqual(self.controller.url, "https://[2001:db8::1]:8443/")
+        self.controller.requester.set_url.assert_called_once_with(
+            "https://[2001:db8::1]:8443/"
+        )
